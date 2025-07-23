29dcb6584eeedf2e7ed8c207f3dc1834a633a474
Dhurjoy Paul <dev.dhurjoy@gmail.com>
2025-07-23 17:58:58 +0530

    Added JWT related things

--- index.js
+++ index.js
@@ -24,7 +24,21 @@ app.use(express.json())
 app.use(cookieParser())
 
 // jwt token verification middleware
+const verifyToken = async (req, res, next) => {
+  const token = req.cookies?.token
 
+  if (!token) {
+    return res.status(401).send({ message: 'unauthorized access' })
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      console.log(err)
+      return res.status(401).send({ message: 'unauthorized access' })
+    }
+    req.user = decoded
+    next()
+  })
+}
 
 // MongoDB Setup
 const client = new MongoClient(process.env.MONGODB_URI, {
@@ -43,6 +57,19 @@ async function run() {
   try {
     // await client.connect();
 
+    // Generate jwt token
+    app.post('/jwt', async (req, res) => {
+      const email = req.body
+      const token = jwt.sign(email, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: '365d',
+      })
+      res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+      })
+        .send({ success: true })
+    })
 
   } finally {
     // 
